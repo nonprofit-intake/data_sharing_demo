393ea3dde4903556fbf0ddf890a1cbd5ebef7e83
sapinspys <garri009@fiu.edu>
2020-09-27 15:30:21 -0400

    Created basic form structure

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <header>Test</header>
+      <div className="inputs">
+        Input
+        <input type="text" name="last_names" />
+        <input type="text" name="ssn" />
+        <button type="submit">Submit</button>
+      </div>
+      <div className="outputs">
+        Outputs
+      </div>
     </div>
   );
 }
