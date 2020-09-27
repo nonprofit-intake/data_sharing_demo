491b07ed68b081ddd11426f9cb32468d84269e1d
sapinspys <garri009@fiu.edu>
2020-09-27 16:07:03 -0400

    Improved form, added labels, and state logic

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [lastName, setLastName] = useState("");
+  const [ssn, setSSN] = useState("");
+
   return (
     <div className="App">
       <header>Test</header>
-      <div className="inputs">
-        Input
-        <input type="text" name="last_names" />
-        <input type="text" name="ssn" />
+      <form className="inputs">
+        <label htmlFor="">
+          Last Name(s):
+          <input
+            type="text"
+            name="lastName"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+        <label htmlFor="">
+          SSN(s):
+          <input
+            type="text"
+            name="ssn"
+            value={ssn}
+            onChange={(event) => setSSN(event.target.value)}
+          />
+        </label>
         <button type="submit">Submit</button>
-      </div>
+      </form>
       <div className="outputs">
-        Outputs
+        <p>{lastName}</p>
+        <p>{ssn}</p>
       </div>
     </div>
   );
