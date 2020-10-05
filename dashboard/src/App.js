b65183613bcfdf88c1199d43761fdfe38560125b
sapinspys <garri009@fiu.edu>
2020-10-05 14:10:28 -0400

    styled cards and container

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,36 +8,48 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const temp_response = {
-  'complete_matches': [{'enroll_date': '11-18-2018',
-   'exit_date': '01-13-2019',
-   'exit_destination': 'Rental by client, other ongoing housing subsidy',
-   'first_name': 'britney',
-   'income_at_entry': null,
-   'income_at_exit': 569.0,
-   'last_name': 'estabrooks'},
-  {'enroll_date': '06-14-2018',
-   'exit_date': '07-28-2018',
-   'exit_destination': 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter',
-   'first_name': 'britney',
-   'income_at_entry': null,
-   'income_at_exit': 459.0,
-   'last_name': 'estabrooks'},
-  {'enroll_date': '10-25-2018',
-   'exit_date': '11-18-2018',
-   'exit_destination': 'Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter',
-   'first_name': 'britney',
-   'income_at_entry': 569.0,
-   'income_at_exit': 569.0,
-   'last_name': 'estabrooks'},
-  {'enroll_date': '12-01-2018',
-   'exit_date': null,
-   'exit_destination': null,
-   'first_name': 'britney',
-   'income_at_entry': null,
-   'income_at_exit': null,
-   'last_name': 'estabrooks'}],
- 'partial_matches': [{'first_name': 'liliya', 'last_name': 'kryshtal'}]
-}
+  complete_matches: [
+    {
+      enroll_date: "11-18-2018",
+      exit_date: "01-13-2019",
+      exit_destination: "Rental by client, other ongoing housing subsidy",
+      first_name: "britney",
+      income_at_entry: null,
+      income_at_exit: 569.0,
+      last_name: "estabrooks",
+    },
+    {
+      enroll_date: "06-14-2018",
+      exit_date: "07-28-2018",
+      exit_destination:
+        "Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter",
+      first_name: "britney",
+      income_at_entry: null,
+      income_at_exit: 459.0,
+      last_name: "estabrooks",
+    },
+    {
+      enroll_date: "10-25-2018",
+      exit_date: "11-18-2018",
+      exit_destination:
+        "Emergency shelter, including hotel or motel paid for with emergency shelter voucher, or RHY-funded Host Home shelter",
+      first_name: "britney",
+      income_at_entry: 569.0,
+      income_at_exit: 569.0,
+      last_name: "estabrooks",
+    },
+    {
+      enroll_date: "12-01-2018",
+      exit_date: null,
+      exit_destination: null,
+      first_name: "britney",
+      income_at_entry: null,
+      income_at_exit: null,
+      last_name: "estabrooks",
+    },
+  ],
+  partial_matches: [{ first_name: "liliya", last_name: "kryshtal" }],
+};
 
 function App() {
   const [lastName, setLastName] = useState("");
@@ -111,7 +123,10 @@ function App() {
             value={ssn}
             onChange={(event) => setSSN(event.target.value)}
           />
-          <Form.Text>*Last name order must match SSN order. Loading will take 10+ seconds.</Form.Text>
+          <Form.Text>
+            *Last name order must match SSN order. Loading will take 10+
+            seconds.
+          </Form.Text>
         </Form.Group>
         <Button type="submit">Find Matches</Button>
       </Form>
@@ -120,21 +135,21 @@ function App() {
         {isLoading ? (
           <p>loading....</p>
         ) : (
-          matches.complete_matches.map((match, i) => (
-            <Card
-              bg="success"
-              key={i}
-              text="white"
-              style={{ width: '18rem' }}
-            >
-              <Card.Header>Full Match</Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  {JSON.stringify(match)}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))
+          <div className="cardContainer">
+            {matches.complete_matches.map((match, i) => (
+              <Card
+                bg="success"
+                key={i}
+                text="white"
+                className="card"
+              >
+                <Card.Header>Full Match</Card.Header>
+                <Card.Body>
+                  <Card.Text>{JSON.stringify(match)}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
         )}
       </div>
     </Container>
