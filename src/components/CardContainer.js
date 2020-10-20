9cf3929cc75b3c3aa29c2abf6f3c9fb036b7b5a2
sapinspys <garri009@fiu.edu>
2020-10-19 23:52:53 -0400

    Constructed custom CardContainer Component

---
+++ src/components/CardContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./CardContainer.css";
+
+import ResultsCard from "./components/ResultsCard";
+
+function CardContainer(props) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <div className="cardContainer">
+        {props.matchData.partial_matches.map((match, i) => (
+          <ResultsCard
+            key={i}
+            textColor="white"
+            cardColor="#006FBA"
+            matchData={match}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default CardContainer;
